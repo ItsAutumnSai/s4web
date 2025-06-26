bac8f068cb858713afd363e0fd84c8b1078e430b
habu <agunghabuu@gmail.com>
2025-06-26 08:24:04 +0700

    template

---
+++ Pertemuan14/petani_asik/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\SensorController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/kirim_sensor', [SensorController::class, 'store']);
+Route::get('/bacajson/{iot_id}', [SensorController::class, 'getDataLidar']);
+
+
